d601c41c53e03d348ea793d0999a49e5ac9907aa
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-03 14:54:21 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+
+    /* PROGRAM FLOW:
+    -> Ask for input
+    -> Compute value
+    -> Output value
+    -> Repeat on loop until "END" is typed or file reading has ended
+    */
+
+    return 0;
+}
