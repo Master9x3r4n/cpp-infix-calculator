281544fe30a0b280b00ceb618227ae7cb179298f
Stephen <stephen.jszs.gplay@gmail.com>
2026-03-04 11:24:49 +0800

    feat: integrated different modules together

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include "Data.cpp"
 #include "InfixData.cpp"
 #include "PostfixData.cpp"
 
 using namespace std;
+
+//This method prints out the contents of the queue
+void displayQueue(queue<Data> data) {
+    int length = data.size();
+    for (int j = 0; j < length; j ++) 
+    {
+        Data temp = data.front();
+        data.pop();
+        data.push(temp);
+        
+        if (temp.isOperand())
+            cout << temp.getOperand() << " ";
+        else
+            cout << temp.getOperator() << " ";
+        }
+    cout << "\n";
+}
 
 int main(){
     string input;
@@ -19,13 +37,21 @@ int main(){
         
         //Convert string to infix data
         InfixData infix = InfixData(input);
-        cout << "Input: " + input << endl;
+        cout << "Input: " + input << "\n";
 
         //Convert the infix to postfix
-        PostfixData postfix = PostfixData(infix);
+        PostfixData postfix = PostfixData(infix.getData());
+        cout << "Postfix: ";
+        displayQueue(postfix.getData());
 
         //Evaluate postfix
-        infix.displayQueue();
+        Data result = postfix.evaluate();
+
+        cout << "\nResult: ";
+        if (result.isOperand())
+            cout << result.getOperand() << "\n";
+        else
+            cout << result.getOperator() << "\n";
     }
 
     return 0;
